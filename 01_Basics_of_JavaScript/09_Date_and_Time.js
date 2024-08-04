a074cb0d64c5818899d9f17ad45a8a15fc66c183
meetwithanshul <134866369+meetwithanshul@users.noreply.github.com>
2024-08-04 12:12:11 +0000

    dates and time in JS

---
+++ 01_Basics_of_JavaScript/09_Date_and_Time.js
@@ -0,0 +1,10 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleTimeString())
+
+console.log(typeof date)
