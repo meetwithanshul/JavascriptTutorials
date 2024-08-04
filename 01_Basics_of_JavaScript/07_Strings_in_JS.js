d435b983a68015c37d40b9540c26d964ad81022f
meetwithanshul <134866369+meetwithanshul@users.noreply.github.com>
2024-08-04 11:35:50 +0000

    till strings in JS

---
+++ 01_Basics_of_JavaScript/07_Strings_in_JS.js
@@ -0,0 +1,37 @@
+// https://www.w3schools.com/js/js_string_methods.asp
+
+let name = "Anshul"
+let sname = "Sharma"
+let age = Number(23)
+
+// console.log(`Name is ${name} ${sname} and age is ${age}`)
+
+let myName = new String("Anshul Sharma")
+let Sname = "Sharma"
+
+// console.log(Sname.toUpperCase())
+// console.log(myName.toUpperCase())
+
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// // console.log(gameName.charAt(2));
+// // console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne = "   Anshul    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://google.com"
+
+console.log(url.replace('goo', '-'))
+
+console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
+
