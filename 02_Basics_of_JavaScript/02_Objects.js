6576ba5954d3327b4e8b1aad6cc6bde541e4528a
meetwithanshul <134866369+meetwithanshul@users.noreply.github.com>
2024-08-06 09:34:04 +0000

    Initial commit

--- 02_Basics_of_JavaScript/02_Objects.js
+++ 02_Basics_of_JavaScript/02_Objects.js
@@ -19,14 +19,83 @@ const user = {
 
 // console.log(user)
 
-user.greeting = function(){
-    console.log("Hello!")
+// user.greeting = function(){
+//     console.log("Hello!")
+// }
+
+// user.greetingTwo = function(){
+//     console.log(`Hello, ${this.name}`)
+// }
+
+// // console.log(user.greeting)
+// console.log(user.greeting())
+// console.log(user.greetingTwo())
+
+// *****************************************************************************************************
+
+// const tinderUser = new Object() // Singleton object
+const tinderUser = {} // Non-singleton object
+
+tinderUser.id = "23MCA1054"
+tinderUser.name = "Anshul"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email : "abc@gmail.com",
+    fullname : {
+        firstName : "Anshul",
+        secondName : "Sharma"
+    },
+    phone : {
+        Fathers : "9887409402",
+        Home : "7424801320"
+    }
 }
 
-user.greetingTwo = function(){
-    console.log(`Hello, ${this.name}`)
-}
+// console.log(regularUser.fullname)
+// console.log(regularUser.phone)
 
-// console.log(user.greeting)
-console.log(user.greeting())
-console.log(user.greetingTwo())
+const obj1 = {1 : "A", 2 : "B"}
+const obj2 = {3 : "C", 4 : "D"} 
+// const obj3 = Object.assign({}, obj1, obj2) // {} means all values will be combined and guarenteed result will be an object
+// {} -> target and obj1.... source
+// console.log(obj3)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3)
+
+
+const users = [
+    {
+        id : 101,
+        name : "B"
+    },
+    {
+        id : 102,
+        name : "C"
+    },
+    {
+        id : 103,
+        name : "D"
+    },
+    {
+        id : 104,
+        name : "E"
+    }
+
+]
+
+// user[1].email
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+console.log(tinderUser.hasOwnProperty('isLoggedOut'))
+
+
+// *******************************************************************************************************************
+
+// Objects Destructuring
+
